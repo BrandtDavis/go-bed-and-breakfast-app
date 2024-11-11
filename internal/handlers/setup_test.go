1e99c7e5a6fbed93ed71254dfae3780439e4f91e
Brandt Davis <brandtd77@gmail.com>
2024-11-11 10:31:19 -0700

    Update test setups, clean up and use error logging to handlers package

--- internal/handlers/setup_test.go
+++ internal/handlers/setup_test.go
@@ -9,6 +9,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"time"
 
@@ -21,6 +22,7 @@ import (
 var functions = template.FuncMap{}
 
 var app config.AppConfig
+
 var session *scs.SessionManager
 var pathToTemplates = "./../../templates"
 
@@ -30,6 +32,12 @@ func getRoutes() http.Handler {
 
 	// change this to true when in production
 	app.InProduction = false
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
 
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
