cb5472e6a5f9bc6897cf4c8ca3e07cc0e707bbce
Brandt Davis <brandtd77@gmail.com>
2024-11-21 22:10:37 -0700

    Implement repository pattern for DB-related functionality

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bookings/internal/config"
+	"bookings/internal/driver"
 	"bookings/internal/handlers"
 	"bookings/internal/models"
 	"bookings/internal/render"
@@ -24,10 +25,11 @@ var errorLog *log.Logger
 
 // main is the main application function
 func main() {
-	err := run()
+	db, err := run()
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer db.SQL.Close()
 
 	fmt.Printf("Starting application on port %s", portNumber)
 	srv := &http.Server{
@@ -40,7 +42,7 @@ func main() {
 
 }
 
-func run() error {
+func run() (*driver.DB, error) {
 	// what am I going to put in the session
 	gob.Register(models.Reservation{})
 
@@ -61,18 +63,25 @@ func run() error {
 
 	app.Session = session
 
+	// connect to database
+	log.Println("Connecting to database...")
+	db, err := driver.ConnectSQL("host=localhost port=5432 dbname=bookings user=brandtdavis password=")
+	if err != nil {
+		log.Fatal("Cannot connect to database!  Dying...")
+	}
+
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
-		return err
+		return nil, err
 	}
 
 	app.TemplateCache = tc
 	app.UseCache = false
-	repo := handlers.NewRepo(&app)
+	repo := handlers.NewRepo(&app, db)
 	handlers.NewHandlers(repo)
 
 	render.NewTemplates(&app)
 
-	return nil
+	return db, nil
 }
---
+++ internal/repository/dbrepo/dbrepo.go
@@ -0,0 +1,20 @@
+package dbrepo
+
+import (
+	"bookings/internal/config"
+	"bookings/internal/repository"
+	"database/sql"
+)
+
+type postgresDBRepo struct {
+	App *config.AppConfig
+	DB  *sql.DB
+}
+
+// NewPostgresRepo
+func NewPostgresRepo(conn *sql.DB, a *config.AppConfig) repository.DatabaseRepo {
+	return &postgresDBRepo{
+		App: a,
+		DB:  conn,
+	}
+}
