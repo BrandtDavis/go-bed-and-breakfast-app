36068e93a8fdfb0ce3418c7649f1ab44adc78f19
Brandt Davis <brandtd77@gmail.com>
2024-11-28 23:51:45 -0700

    Add  comment

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -41,6 +41,7 @@ func (m *postgresDBRepo) InsertReservation(res models.Reservation) (int, error) 
 	return newID, nil
 }
 
+// InsertRoomRestriction inserts a room restriction into the DB
 func (m *postgresDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
