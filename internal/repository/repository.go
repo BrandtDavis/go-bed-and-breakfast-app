268b80b48915569861e869406f5ae86fbfb88bc7
Brandt Davis <brandtd77@gmail.com>
2024-11-29 00:05:40 -0700

    Add functionality to search room availability by id

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,10 +1,14 @@
 package repository
 
-import "bookings/internal/models"
+import (
+	"bookings/internal/models"
+	"time"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
 
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDates(start, end time.Time) (bool, error)
 }
