21eba68e61fc1eb2e073cdc60ac2e756a8a63d80
Brandt Davis <brandtd77@gmail.com>
2024-11-30 10:50:12 -0700

    Add migration, add query to find all room availabilities for given dates

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -10,5 +10,6 @@ type DatabaseRepo interface {
 
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) error
-	SearchAvailabilityByDates(start, end time.Time) (bool, error)
+	SearchAvailabilityByDatesByRoomID(start, end time.Time) (bool, error)
+	SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error)
 }
